0be074cae0c3d0981b9a981ace2ddf0c140eb725
Kacao Cao <kacao@iastate.edu>
2018-07-24 17:31:14 -0400

    plot Price as Y parameter in term of other parameters as X

--- boston-regression.py
+++ boston-regression.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-import sklearn
+from sklearn.linear_model import LinearRegression
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-
 # Retrieve data as a dictionary object from a given database
 from sklearn.datasets import load_boston
 boston = load_boston()
@@ -12,9 +11,29 @@ print (boston.keys())
 print (boston['feature_names'])
 
 # load the dataset into Dataframe with a default column names
-bs = pd.DataFrame( boston.data )
-bs.columns = boston['feature_names']
-print (bs.head())
+bos = pd.DataFrame( boston.data )
+bos.columns = boston['feature_names']
+print (bos.head())
 
 # first 5 value of the target: housing price
 print (boston['target'][:5])
+bos['PRICE'] = boston['target']
+
+# keep the dataframe but 'PRICE' column as X parameter
+X = bos.drop( 'PRICE', axis=1 )
+lm = LinearRegression()
+lm.fit( X, bos['PRICE'])
+
+print ("Estimated intercepted: ", lm.intercept_)
+print ("Estimated numbers of coefficient: ", len(lm.coef_))
+
+# output = pd.DataFrame( zip(X.columns, lm.intercept_),
+#                        columns=['features', 'estimated coeff'] )
+# print (output)
+
+#plot the data
+plt.scatter( bos['RM'], bos['PRICE'])
+plt.xlabel("Avg of Room per Dwelling RM")
+plt.ylabel("")
+plt.title("Relationship btw House Price and RM")
+plt.show()
