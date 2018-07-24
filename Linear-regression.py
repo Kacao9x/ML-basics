251daf05160591bfbf93119465f785efd3de2ac9
Kacao Cao <kacao@iastate.edu>
2018-07-24 16:04:18 -0400

    simple machine learning test

---
+++ Linear-regression.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
+from sklearn import svm
+# import scipy
+
+x = [1, 5, 1.5, 8, 1, 9]
+y = [2, 8, 1.8, 8, 0.6, 11]
+# plt.scatter(x,y)
+# plt.show()
+
+X = np.array([[1,2],
+             [5,8],
+             [1.5,1.8],
+             [8,8],
+             [1,0.6],
+             [9,11]])
+
+y = [0,1,0,1,0,1]
+
+clf = svm.SVC(kernel='linear', C = 1.0)
+clf.fit(X,y)
+
+
+
+# example_measures = np.array([4,2,1,1,1,2,3,2,1])
+# example_measures = example_measures.reshape(1, -1)
+# prediction = clf.predict(example_measures)
+# print(prediction)
+
+z = np.array([[0.58, 0.76]])
+z.reshape(1,-1)
+print (clf.predict(z))
+
+z = np.array([[0.58, 0.76], [10.58,10.76]])
+z.reshape(len( z ),-1)
+print (clf.predict(z))
+#
+# print(clf.predict([10.58,10.76]))
+
+w = clf.coef_[0]
+print(w)
+
+a = -w[0] / w[1]
+
+xx = np.linspace(0,12)
+yy = a * xx - clf.intercept_[0] / w[1]
+
+h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+
+plt.scatter(X[:, 0], X[:, 1], c = y)
+plt.legend()
+plt.show()
